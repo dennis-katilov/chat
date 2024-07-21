be9384dd92ce0945c5df0872aea224d8ec93430f
Denns <dennis.katilov@gmail.com>
2024-07-21 17:09:41 +0300

    bind

--- server.cpp
+++ server.cpp
@@ -7,6 +7,9 @@
 #include <netdb.h>
 #include <netinet/in.h>
 
+#define DEFAULT_PORT 1601
+#define ERROR_S "SERVER ERROR "
+
 using namespace std;
 
 int main(int argc, char const* argv[]){
@@ -18,10 +21,19 @@ int main(int argc, char const* argv[]){
     client = socket(AF_INET, SOCK_STREAM, 0);
 
     if (client<0){
-        cout<< "SERVER ERROR: establishing socket error.";
+        cout<< ERROR_S << ": establishing socket error";
         exit(0);
     }
 
     cout<< "SERVER: socket was created";
+
+    server_address.sin_port = htons(DEFAULT_PORT);
+    server_address.sin_family = AF_INET;
+    server_address.sin_addr.s_addr = htons(INADDR_ANY);
+
+    int ret = bind(client, static_cast<struct sock_addr*>(&server_address),sizeof(server_address));
+     if (ret<0){
+        cout<< ERROR_S << "binding connection";
+     }
 }
 
