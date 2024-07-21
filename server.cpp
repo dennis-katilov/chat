b447ccc74f5a6f33b63263dd6f59463114c6b780
Denns <dennis.katilov@gmail.com>
2024-07-21 16:42:40 +0300

    lib and start

--- server.cpp
+++ server.cpp
@@ -1,3 +1,17 @@
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+
+
 int main(int argc, char const* argv[]){
+    int server;
+    int client;
+
+    struct sockaddr_in server_address;
     
+    client = socket(AF_INET, SOCKET_STREAM, 0);
 }
