be65c720267bb6292f533f3c0a20bc176f1e788c
Denns <dennis.katilov@gmail.com>
2024-07-26 20:14:30 +0300

    start to client

--- client.cpp
+++ client.cpp
@@ -1,3 +1,25 @@
-int main(int argc, char const* argv[])){
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <string.h>
+
+
+bool is_client_connection_close(const char* msg);
+
+int main(int argc, char const* argv[]){
     return 0;
+}
+
+bool is_client_connection_close(const char* msg){
+    for (int i=0; i<strlen(msg); ++i){
+        if (msg[i]=='#'){
+            return true;
+        }
+    }
+    return false;
 }
