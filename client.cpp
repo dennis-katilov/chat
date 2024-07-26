95c0553bfa62200e03d8ced690ff5052a1bdb133
Denns <dennis.katilov@gmail.com>
2024-07-26 20:26:22 +0300

    connect client

--- client.cpp
+++ client.cpp
@@ -8,10 +8,32 @@
 #include <netinet/in.h>
 #include <string.h>
 
+using namespace std;
+
+#define SERVER_IP "127.0.0.1"
+#define DEFAULT_PORT 1601
+#define ERROR_S "SERVER ERROR "
+#define SERVER_CLOSE_CONNECTION_SYMBOL "#"
 
 bool is_client_connection_close(const char* msg);
 
 int main(int argc, char const* argv[]){
+    int client;
+
+    struct sockaddr_in server_address;
+
+    client = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (client<0){
+        cout<< ERROR_S << ": establishing socket error";
+        exit(0);
+    }
+
+    server_address.sin_port = htons(DEFAULT_PORT);
+    server_address.sin_family = AF_INET;
+    inet_pton(AF_INET, SERVER_IP, &server_address.sin_addr);
+
+    cout<<"=> Client socked was created";
     return 0;
 }
 
